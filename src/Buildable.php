5531ff2d0421cb771df584bfafdf474c3cdb194f
Mitirrli <Mitirrli@users.noreply.github.com>
2020-10-01 14:40:46 +0000

    Apply fixes from StyleCI

--- src/Buildable.php
+++ src/Buildable.php
@@ -45,12 +45,12 @@ trait Buildable
     /**
      * assign the value.
      *
-     * @param array|string $key can be array or string
-     * @param int $fuzzy fuzzy search
-     *
-     * @return $this
+     * @param array|string $key   can be array or string
+     * @param int          $fuzzy fuzzy search
      *
      * @throws Exception\NotExistException
+     *
+     * @return $this
      *
      * @example
      * <pre>
@@ -97,8 +97,8 @@ trait Buildable
     /**
      * Between two keys.
      *
-     * @param string $key name of key
-     * @param array $value array of value
+     * @param string $key   name of key
+     * @param array  $value array of value
      *
      * @return $this
      *
@@ -122,7 +122,7 @@ trait Buildable
     /**
      * before one key.
      *
-     * @param string $key name of key
+     * @param string      $key  name of key
      * @param string|null $name final name of the key
      *
      * @return $this
@@ -139,7 +139,7 @@ trait Buildable
     /**
      * after one key.
      *
-     * @param string $key name of key
+     * @param string      $key  name of key
      * @param string|null $name final name of the key
      *
      * @return $this
