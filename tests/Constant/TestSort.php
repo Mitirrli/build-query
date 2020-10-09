7e03ed61e076446d802addb39076dfb5f6837440
mitirrli <phper.blue@gmail.com>
2020-10-09 17:00:32 +0800

    Add sortable unit constant.

---
+++ tests/Constant/TestSort.php
@@ -0,0 +1,18 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Mitirrli\Buildable\Tests\Constant;
+
+abstract class TestSort
+{
+    const TEST_SORT1 = [
+        'create_time' => 'desc'
+    ];
+
+    const TEST_SORT2 = [
+        'create_time' => 'desc',
+        'update_time' => 'asc',
+        'sort' => 'asc'
+    ];
+}
