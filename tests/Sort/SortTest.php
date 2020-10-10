8b381d120553106e11eb59c9fc03638bc6977d25
mitirrli <phper.blue@gmail.com>
2020-10-10 11:17:47 +0800

    Add unit test.

--- tests/Sort/SortTest.php
+++ tests/Sort/SortTest.php
@@ -23,8 +23,8 @@ class SortTest extends TestCase
         $test1 = $this->param(TestSort::TEST_SORT1)->sort('create_time')->result();
 
         self::assertIsArray($test1);
-        self::assertArrayHasKey('sort', $test1);
-        self::assertEquals($key.' '.TestSort::TEST_SORT1[$key], $test1['sort']);
+        self::assertArrayNotHasKey('sort', $test1);
+        self::assertEquals($key.' '.TestSort::TEST_SORT1[$key], $this->order());
 
         //Test 2. rename sort
         $key = 'create_time';
@@ -32,8 +32,8 @@ class SortTest extends TestCase
         $test2 = $this->initial([])->param(TestSort::TEST_SORT1)->sort('create_time', $rename_key)->result();
 
         self::assertIsArray($test2);
-        self::assertArrayHasKey($rename_key, $test2);
-        self::assertEquals($key.' '.TestSort::TEST_SORT1[$key], $test2[$rename_key]);
+        self::assertArrayNotHasKey($rename_key, $test2);
+        self::assertEquals($key.' '.TestSort::TEST_SORT1[$key], $this->order($rename_key));
 
         //Test 3. rename array sort
         $key = 'create_time';
@@ -42,23 +42,23 @@ class SortTest extends TestCase
         $test3 = $this->initial([])->param(TestSort::TEST_SORT1)->sort([$key, $sql_key], $rename_key)->result();
 
         self::assertIsArray($test3);
-        self::assertArrayHasKey($rename_key, $test3);
-        self::assertEquals($sql_key.' '.TestSort::TEST_SORT1[$key], $test3[$rename_key]);
+        self::assertArrayNotHasKey($rename_key, $test3);
+        self::assertEquals($sql_key.' '.TestSort::TEST_SORT1[$key], $this->order($rename_key));
 
         //Test 4. multi sort.
         $key2 = 'update_time';
         $test4 = $this->initial([])->param(TestSort::TEST_SORT2)->sort($key)->sort($key2)->result();
 
         self::assertIsArray($test4);
-        self::assertArrayHasKey('sort', $test4);
-        self::assertEquals($key2.' '.TestSort::TEST_SORT2[$key2], $test4['sort']);
+        self::assertArrayNotHasKey('sort', $test4);
+        self::assertEquals($key2.' '.TestSort::TEST_SORT2[$key2], $this->order());
 
         //Test 5. rename multi sort.
         $test5 = $this->initial([])->param(TestSort::TEST_SORT2)->sort($key, 'num1')->sort($key2, 'num2')->result();
         self::assertIsArray($test5);
-        self::assertArrayHasKey('num1', $test5);
-        self::assertEquals($key2.' '.TestSort::TEST_SORT2[$key2], $test5['num2']);
-        self::assertEquals($key.' '.TestSort::TEST_SORT2[$key], $test5['num1']);
-        self::assertArrayHasKey('num2', $test5);
+        self::assertArrayNotHasKey('num1', $test5);
+        self::assertEquals($key2.' '.TestSort::TEST_SORT2[$key2], $this->order('num2'));
+        self::assertEquals($key.' '.TestSort::TEST_SORT2[$key], $this->order('num1'));
+        self::assertArrayNotHasKey('num2', $test5);
     }
 }
