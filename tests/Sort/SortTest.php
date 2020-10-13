e9007ed7f298b30d8d2f3580c8d5848ec99cd953
mitirrli <phper.blue@gmail.com>
2020-10-13 11:10:40 +0800

    Name not exist throw exception

--- tests/Sort/SortTest.php
+++ tests/Sort/SortTest.php
@@ -2,6 +2,7 @@
 
 namespace Mitirrli\Buildable\Tests\Sort;
 
+use Mitirrli\Buildable\Exception\NotExistException;
 use Mitirrli\Buildable\Sortable;
 use Mitirrli\Buildable\Tests\Constant\TestSort;
 use PHPUnit\Framework\TestCase;
@@ -12,6 +13,7 @@ class SortTest extends TestCase
 
     /**
      * test NotExistException.
+     * @throws NotExistException
      */
     public function testSort()
     {
@@ -20,7 +22,7 @@ class SortTest extends TestCase
         $test1 = $this->param(TestSort::TEST_SORT1)->sort('create_time')->result();
 
         self::assertIsString($test1);
-        self::assertEquals($key.' '.TestSort::TEST_SORT1[$key], $test1);
+        self::assertEquals($key . ' ' . TestSort::TEST_SORT1[$key], $test1);
 
         //Test 2. rename sort
         $key = 'create_time';
@@ -28,7 +30,7 @@ class SortTest extends TestCase
         $test2 = $this->initial([])->param(TestSort::TEST_SORT1)->sort('create_time', $rename_key)->result($rename_key);
 
         self::assertIsString($test2);
-        self::assertEquals($key.' '.TestSort::TEST_SORT1[$key], $test2);
+        self::assertEquals($key . ' ' . TestSort::TEST_SORT1[$key], $test2);
 
         //Test 3. rename array sort
         $key = 'create_time';
@@ -37,19 +39,36 @@ class SortTest extends TestCase
         $test3 = $this->initial([])->param(TestSort::TEST_SORT1)->sort([$key, $sql_key], $rename_key)->result($rename_key);
 
         self::assertIsString($test3);
-        self::assertEquals($sql_key.' '.TestSort::TEST_SORT1[$key], $test3);
+        self::assertEquals($sql_key . ' ' . TestSort::TEST_SORT1[$key], $test3);
 
         //Test 4. multi sort.
         $key2 = 'update_time';
         $test4 = $this->initial([])->param(TestSort::TEST_SORT2)->sort($key)->sort($key2)->result();
 
         self::assertIsString($test4);
-        self::assertEquals($key2.' '.TestSort::TEST_SORT2[$key2], $test4);
+        self::assertEquals($key2 . ' ' . TestSort::TEST_SORT2[$key2], $test4);
 
         //Test 5. rename multi sort.
         $test5 = $this->initial([])->param(TestSort::TEST_SORT2)->sort($key, 'num1')->sort($key2, 'num1')->result('num1');
 
         self::assertIsString($test5);
-        self::assertEquals($key2.' '.TestSort::TEST_SORT2[$key2], $test5);
+        self::assertEquals($key2 . ' ' . TestSort::TEST_SORT2[$key2], $test5);
+    }
+
+    /**
+     * test sort not exist.
+     *
+     * @throws NotExistException
+     */
+    public function testSortNotExist()
+    {
+        $key = 'create_time';
+        $key2 = 'update_time';
+
+        $this->expectException(NotExistException::class);
+        $this->expectExceptionMessage('This key is not exist.');
+        $this->expectExceptionCode(2);
+
+        $this->initial([])->param(TestSort::TEST_SORT2)->sort($key, 'num1')->sort($key2, 'num1')->result('num2');
     }
 }
