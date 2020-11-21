29f00f976b0310c1eb691efe62d0e9c4e77cfae1
mitirrli <phper.blue@gmail.com>
2020-11-22 00:15:43 +0800

    Compatibility.

--- tests/MultiInKey.php
+++ tests/MultiInKey.php
@@ -6,14 +6,18 @@ class MultiInKey
 {
     public function a(array $data)
     {
-        $result = array_map(fn ($key) => $key * $key, $data);
+        $result = array_map(function ($key) {
+            return $key * $key;
+        }, $data);
 
         return $result;
     }
 
-    public function b(array $value)
+    public function b(array $data)
     {
-        $result = array_map(fn ($key) => $key + $key - 2, $value);
+        $result = array_map(function ($key) {
+            return 2 * $key - 2;
+        }, $data);
 
         return $result;
     }
